d9a59f51f5498af8f2657a764a3d339b98630549
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-29 11:56:20 +0600

    Generate Password Service

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+/**
+ * @author Yeasir Arafat Aronno
+ */
+
 public class Main {
+    public static final Scanner
+            userINPUT = new Scanner(System.in); // Create a Scanner object.
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Generator generator = new Generator(userINPUT); // Create a Generator object.
+        generator.mainUserWindow(); // Call the mainLoop method.
+        userINPUT.close(); // Close the Scanner object.
     }
 }
